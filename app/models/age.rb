29f7b57c8bab68063d3027e23a8256f0e3f9fdee
mameton <out.of.escape@outlook.jp>
2020-10-07 18:41:13 +0900

    ageの変更

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,11 +1,11 @@
 class Age < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '10〜20代' },
-    { id: 3, name: '20〜30代' },
-    { id: 4, name: '30〜40代' },
-    { id: 5, name: '40〜50代' },
-    { id: 6, name: '50〜60代' },
-    { id: 7, name: '60〜70代' }
+    { id: 2, name: '10〜19歳' },
+    { id: 3, name: '20〜29歳' },
+    { id: 4, name: '30〜39歳' },
+    { id: 5, name: '40〜49歳' },
+    { id: 6, name: '50〜59歳' },
+    { id: 7, name: '60歳〜' }
   ]
 end
